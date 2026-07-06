927a8f42708f40922d2fa676cc9ce747a632e29e
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 11:45:29 +0530

    swap completed

---
+++ Day-01/programs/SwapNum.java
@@ -0,0 +1,12 @@
+public class SwapNum {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("A:"+a);
+        System.out.println("B:"+b);
+
+    }
+}
