edc010c70aff8f38f55bbd2b82b8c19810e7438c
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 11:14:45 +0530

    Address prgm completed

---
+++ Day-01/programs/Address.java
@@ -0,0 +1,13 @@
+public class Address {
+    public static void main(String[] args){
+        System.out.println("House no :7-328");
+        System.out.println("Colony: Lakshmi nagar colony ");
+        System.out.println("Area: Nagaram,Dammaiguda");
+        System.out.println("District: Medchal-Malkajgiri");
+        System.out.println("city : Hyderabad");
+        System.out.println("pincode: 500083");
+        System.out.println("State: Telangana");
+
+
+    }
+}
