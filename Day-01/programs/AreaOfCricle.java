73da334b8abbc5ced4616f6a91ee45171cc02b78
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 11:55:52 +0530

    area2 completed

---
+++ Day-01/programs/AreaOfCricle.java
@@ -0,0 +1,7 @@
+public class AreaOfCricle {
+    public static void main(String[] args){
+        int radius=10;
+        double area=3.14*radius*radius;
+        System.out.println("Area of circle="+area);
+    }
+}
