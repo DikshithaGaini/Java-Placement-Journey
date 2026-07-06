6b9eccf95c28fc231b22b18be334ae73c48d99c7
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 10:59:36 +0530

    Clginfo completed

---
+++ Day-01/programs/CollegeInfo.java
@@ -0,0 +1,7 @@
+public class CollegeInfo {
+    public static void main(String[] args){
+        System.out.println(" Name:Dikshitha");
+        System.out.println(" College: ACE engineering college");
+        System.out.println(" Branch: Information Technology");
+    }
+}
