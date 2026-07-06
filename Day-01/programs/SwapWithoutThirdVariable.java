fccdcb16d26ffcd9f2bedc3c891d8b04b42818c5
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 11:48:32 +0530

    swap2 completed

---
+++ Day-01/programs/SwapWithoutThirdVariable.java
@@ -0,0 +1,12 @@
+public class SwapWithoutThirdVariable {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("A:"+a);
+        System.out.println("B:"+b);
+
+    }
+}
