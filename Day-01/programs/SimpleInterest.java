8fb1e7d0fa4a3574d8fc3b473286e98dc1db56b8
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 14:01:22 +0530

    simpleinterest completed

---
+++ Day-01/programs/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+    public static void main(String[] args){
+        int p=1000;
+        int t=2;
+        int r=5;
+        int s=(p*r*t)/100;
+        System.out.println("Simple Interest:"+s);
+    }
+}
