3f4c2244bd4912521deac1767c0414827e28380f
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 14:41:02 +0530

    primitive completed

---
+++ Day-01/programs/PrimitiveDataTypesDemo.java
@@ -0,0 +1,28 @@
+public class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 20000;
+        int i = 500000;
+        long l = 9876543210L;
+
+        float f = 12.34f;
+        double d = 12345.6789;
+
+        char ch = 'A';
+
+        boolean isJavaFun = true;
+
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+
+        System.out.println("Character value: " + ch);
+
+        System.out.println("Boolean value: " + isJavaFun);
+    }
+}
