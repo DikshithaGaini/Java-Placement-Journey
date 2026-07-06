2b446a59fb7f76bb196be39b6bad5dcb13f431da
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 13:54:41 +0530

    widening completed

---
+++ Day-01/programs/Widening.java
@@ -0,0 +1,8 @@
+public class Widening {
+    public static void main(String[] args){
+        int a=10;
+        double b=a;
+        System.out.println("A:"+a);
+        System.out.println("B:"+b);
+    }
+}
