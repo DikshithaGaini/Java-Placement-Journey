8664995f02c59722d08e1f3f4e80ef5f40b3d5b0
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-06 13:59:27 +0530

    typecasting completed

---
+++ Day-01/programs/TypeCast.java
@@ -0,0 +1,9 @@
+public class TypeCast {
+    public static void main(String[] args){
+        double b=10.5;
+        int a=(int)b;
+        System.out.println("A:"+a);
+        System.out.println("B:"+b);
+
+    }
+}
