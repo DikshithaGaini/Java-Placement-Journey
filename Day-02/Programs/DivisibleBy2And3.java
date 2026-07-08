834c6c85233038607cd10d518192d8a8d0b28739
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-08 14:50:03 +0530

    Day-02 completed

--- Day-02/Programs/DivisibleBy2And3.java
+++ Day-02/Programs/DivisibleBy2And3.java
@@ -1,7 +1,7 @@
 public class DivisibleBy2And3 {
      public static void main(String[] args){
         int num=132;
-        if(num%2==0 && num%3git==0){
+        if(num%2==0 && num%3==0){
             System.out.println("Divisible");
         }
         else{
