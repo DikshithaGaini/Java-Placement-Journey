d5055ef3ff797a318ec3ab79165ec2aeba56faff
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-14 23:15:20 +0530

    completed program 2

---
+++ Day-05/Programs/SumArray.java
@@ -0,0 +1,12 @@
+public class SumArray {
+    public static void main(String[] args){
+    int[] arr={2,3,4,5,6};
+    int sum=0;
+    System.out.println("Array sum");
+    for(int i=0;i<arr.length;i++){
+        sum=sum+arr[i];
+    }
+        System.out.print(sum);
+    
+    }
+}
