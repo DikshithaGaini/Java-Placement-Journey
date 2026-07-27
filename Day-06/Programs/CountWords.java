29a5a55900e5aea56827acab725672c52ddd3e1c
Dikshitha Gaini <gainidikshitha@gmail.com>
2026-07-27 22:38:57 +0530

    Completed 1 program

---
+++ Day-06/Programs/CountWords.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String str = sc.nextLine();
+
+        str = str.trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = str.split("\\s+");
+            System.out.println("Number of words: " + words.length);
+        }
+
+        sc.close();
+    }
+}
